4ba19f7365da90c5d1af04becafbf4b59c41c664
zyd <1490209517@qq.com>
2020-08-10 11:47:14 +0800

    first commit

---
+++ src/main/java/com/zyd/git/HelloGit.java
@@ -0,0 +1,9 @@
+package com.zyd.git;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello");
+    }
+}
