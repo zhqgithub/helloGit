34f60b30cbeab952e0e02bd93375b1caad9699c0
zyd <1490209517@qq.com>
2020-08-10 11:49:08 +0800

    更新2

--- src/main/java/com/zyd/git/HelloGit.java
+++ src/main/java/com/zyd/git/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("Hello");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
